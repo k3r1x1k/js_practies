4038ee45fb9ba57cae802cb88c14f5bea6abac11
k3r1x1k <kirilshostak2020@gmail.com>
2026-02-03 18:25:09 +0100

    перший урок

---
+++ js/01-variables.js
@@ -0,0 +1,18 @@
+const myName = "Kiril"
+console.log(myName)
+
+const myAge = 14
+console.log(myAge)
+
+console.log(typeof myName)
+
+console.log(typeof myAge)
+
+const producktName = "cup"
+console.log(producktName)
+
+let producktPrice = 5.99
+console.log(producktPrice)
+
+producktPrice = 10
+console.log(producktPrice)

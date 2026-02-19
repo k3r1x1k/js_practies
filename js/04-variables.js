54c9470ab7f5dcfff6d595824eb1a512daa69374
k3r1x1k <kirilshostak2020@gmail.com>
2026-02-19 17:28:21 +0100

    04

---
+++ js/04-variables.js
@@ -0,0 +1,11 @@
+// Welcome guestName, your room number is roomNumber!
+const guestName = "Mango";
+const roomNumber = 207;
+
+const greating = "Welcome" + " " + guestName + ", your room number is" + " " + roomNumber ;
+const greating2 = `Welcome ${guestName}, your room number is ${roomNumber}`;
+console.log(greating2)
+
+
+
+// console.log(greeting);

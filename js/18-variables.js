36c0dec8fd60d31eaddb9c8eaad9e72380318000
k3r1x1k <kirilshostak2020@gmail.com>
2026-05-07 17:08:02 +0200

    18

---
+++ js/18-variables.js
@@ -0,0 +1,21 @@
+// Практика разом деструктуризувати
+// Об’єкт car
+const {make, model: carmodel, year, features = [], safety: {airbags, antilock_brakes, stability_control}} = car
+console.log(make, carmodel, year, features, airbags, antilock_brakes, stability_control);
+
+// Об’єкт movie
+const movie = {
+  title: 'The Shawshank Redemption',
+  director: {
+    name: 'Frank Darabont',
+    nationality: 'American'
+  },
+  actors: ['Tim Robbins', 'Morgan Freeman'],
+  release_year: 1994,
+  ratings: {
+    imdb: 9.3,
+     : 90
+  }
+}
+
+const{ title, director, name, nationality, actors, release_year, ratings, imdbrotten_tomatoes}

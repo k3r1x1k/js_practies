a132debb51fd180965ff60e2eade7c77fcedbcca
k3r1x1k <kirilshostak2020@gmail.com>
2026-06-04 17:59:16 +0200

    25

---
+++ js/DOM/25-variables.js
@@ -0,0 +1,39 @@
+const newList = document.querySelector(".js-list");
+// console.log(newList);
+
+const newItem = document.createElement("li");
+newItem.classList.add("new-item");
+
+const newLinkEl = document.createElement("a");
+newLinkEl.textContent = "Hello";
+
+newItem.appendChild(newLinkEl);
+//console.log(newItem);
+
+newList.appendChild(newItem)
+
+
+const colorPickerContainer = document.querySelector('.js-color-picker');
+// console.log(colorPickerContainer)
+const colorPickerOptions = [
+  { label: 'red', color: '#F44336' },
+  { label: 'green', color: '#4CAF50' },
+  { label: 'blue', color: '#2196F3' },
+  { label: 'grey', color: '#607D8B' },
+  { label: 'pink', color: '#E91E63' },
+  { label: 'indigo', color: '#3F51B5' },
+];
+
+const buttonList = []
+
+for (const color of colorPickerOptions) {
+  const btn = document.createElement("button");
+  btn.textContent = color.label;
+  btn.style.backgroundColor = color.color; 
+  btn.type = "button"
+  buttonList.push(btn)
+}
+
+console.log(buttonList)
+
+colorPickerContainer.append(...buttonList);
